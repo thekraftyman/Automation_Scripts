ddfaff57d7a6316c83514d81817fbd84a76dfec3
thekraftyman <thekraftyman@gmail.com>
2021-04-30 11:55:22 -0500

    finished first iteration of git auto save

--- Mac/auto_git_push.sh
+++ Mac/auto_git_push.sh
@@ -6,7 +6,6 @@ gitdir=$1
 # exit if the dir is empty
 [[ -z "$gitdir" ]] && { echo "Recieved empty dir" | tee -a /tmp/auto_git.log; exit 1; }
 
-
 # cd to dir
 cd $gitdir
 
@@ -23,21 +22,30 @@ if git diff-index --quiet HEAD --; then
 fi
 
 # check to see if the script/auto-push branch exists upstream
-git ls-remote --heads | grep "script/auto-push" > /dev/null
+git ls-remote --heads | grep "script/auto-push$" > /dev/null
 
-# checkout the auto push branch
+# create the auto push branch
+auto_branch_exists="true"
 if [[ "$?" == "1" ]]; then
   # doesn't exist, create it
-  git checkout -b "script/auto-push"
-  branch_exists="false"
-else
-  # just checkout
-  git checkout "script/auto-push"
-  branch_exists="true"
+  git branch "script/auto-push"
+
+  # set var to set upstream later
+  auto_branch_exists="false"
 fi
 
-# fetch any changes
-git fetch && git pull
+# get temporary auto-push branch name to merge with the current auto-push branch
+tmp_auto_branch="script/auto-push-$(date +"%Y%m%d_%H%M%S")"
+
+# stash current changes
+git stash --message "savepoint_1"
+
+# create a second reference to the same stashg
+git stash apply
+git stash push --message "savepoint_2"
+
+# create branch based on stashed changes (switches to that branch automatically)
+git stash branch "$tmp_auto_branch"
 
 # add all of the changes
 git add --all
@@ -45,21 +53,33 @@ git add --all
 # commit changes
 git commit -m "Automatic commit"
 
+# checkout the base script/auto-push branch
+git checkout script/auto-push
 
+# set script/auto-push upstream if it doesn't exist
 if [[ "$branch_exists" == "false" ]]; then
   # doesn't exist, set upstream
   git push --set-upstream origin script/auto-push
-elif [[ "$branch_exists" == "true" ]]; then
-  # just push
-  git push
-else
-  # error... exit
-  echo "Error pushing branch script/auto-push in dir $gitdir" | tee -a /tmp/auto_git.log
-  exit
 fi
+
+# merge the tmp auto-push into the real auto-push
+#   uses a "theirs" strategy so everthing will get
+#   overwritten if there are merge problems
+git checkout --force
+git pull
+git merge -X theirs --message "merge tmp branch $tmp_auto_branch" "$tmp_auto_branch"
+git push
+
+# delete the tmp branch
+git branch -d "$tmp_auto_branch"
 
 # switch back to the old branch if its different
 if [[ $branch != "script/auto-push" ]]; then
   git checkout $branch;
-  git fetch;
 fi
+
+# get the stashed changes
+git stash pop
+
+# fetch changes
+git fetch
